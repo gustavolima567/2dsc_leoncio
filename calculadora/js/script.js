94fdb78e2c90a49b33c00c09fd4165b049760185
gustavolima567 <gustavo.silveira.lima@escola.pr.gov.br>
2024-10-03 15:21:58 +0000

    iniciado o codigo javascript

---
+++ calculadora/js/script.js
@@ -0,0 +1,11 @@
+const numero1 = document.querySelector('#numero1')
+const numero12 = document.querySelector('#numero2')
+const btncalcular = document.querySelector('#btn-calcular')
+const resultado = document.querySelector('.resultado')
+const adicao = document.querySelector('.adicao')
+const subtracao = document.querySelector('.subtracao')
+const multiplicacao = document.querySelector('.multiplicacao')
+const divisao = document.querySelector('.divisao')
+
+alert(numero1)
+
