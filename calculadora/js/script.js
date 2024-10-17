44c88613237bf0383698c90245b99cc2db8adb4a
gustavolima567 <gustavo.silveira.lima@escola.pr.gov.br>
2024-10-17 14:45:57 +0000

    ALTERADO FUNCAO SOMAR

--- calculadora/js/script.js
+++ calculadora/js/script.js
@@ -1,5 +1,5 @@
 const numero1 = document.querySelector('#numero1')
-const numero12 = document.querySelector('#numero2')
+const numero2 = document.querySelector('#numero2')
 const btncalcular = document.querySelector('#btn-calcular')
 const resultado = document.querySelector('.resultado')
 const adicao = document.querySelector('.adicao')
@@ -7,5 +7,24 @@ const subtracao = document.querySelector('.subtracao')
 const multiplicacao = document.querySelector('.multiplicacao')
 const divisao = document.querySelector('.divisao')
 
-alert(numero1)
+function calcular(){
+const n1 = number(numero1.vlue)
+const n2 = number(numero2.vlue)
+if(typeof n1 === 'number'
+    && typeof n2 ==='number'  ){
+        const adicao = `a soma e ${n1} e ${n2} = ${n1+n2}`
+        alert(adicao)
+    } else{
+        alert('por favor,digite um numero correto')
+    }
 
+
+
+
+    const resultado = numero1.value + numero2.value
+    alert('resultado:' + resultado)
+}
+
+btncalcular.addEventListener('click', function(){
+    calcular()
+})
